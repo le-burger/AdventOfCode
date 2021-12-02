f069aa47717947675b1f063626d9759219498f27
burgerl1 <leander.burger@hva.nl>
2021-12-02 10:00:41 +0100

    Day 1 Excercise 1 of Advent of Code 2021

---
+++ src/nl/leburger/Main.java
@@ -0,0 +1,41 @@
+package nl.leburger;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    private static int increased;
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+        try {
+            File myObj = new File("/Users/leanderburger/Development/AdventOfCode/src/nl/leburger/Day1Ex1Input.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                numbers.add(Integer.parseInt(data));
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        System.out.println("The depth is measured " + numbers.size() + " times");
+
+        for (int i = 0; i < numbers.size() - 1; i++) {
+            if (numbers.get(i) < numbers.get(i + 1)) {
+                System.out.println(numbers.get(i));
+                increased++;
+            }
+        }
+
+        System.out.println("The times the depth increased after the previous measurement is " + increased);
+
+    }
+}
